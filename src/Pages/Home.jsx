ad76b5298ccf4653f03e6e1800de8f02094ff11b
unknown <ayzzzch17@gmail.com>
2025-08-21 21:17:09 +0900

    Give Hero Section and About Section

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import "../Pages/Home.css";
 
 import Navbar from "../Component/Navbar";
-// import Hero from "../Component/Hero";
-// import About from "../Component/About";
+import Hero from "../Component/Hero";
+import About from "../Component/About";
 import Talent from "../Component/Talent";
 import News from "../Component/News";
 import Footer from "../Component/Footer";
@@ -16,8 +16,8 @@ const Home = () => (
       <title>Laeta Indonesia</title>
     </Helmet>
     <Navbar />
-    {/* <Hero /> */}
-    {/* <About /> */}
+    <Hero />
+    <About />
     <Talent />
     <News />
     <Footer />
