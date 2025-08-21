d0f2ad305ecd299d6552476eb18ef1eafe786a5a
Ayzu <ayzzzch17@gmail.com>
2025-08-21 21:40:15 +0900

    Update Responsive Hero & About

--- src/Component/About.jsx
+++ src/Component/About.jsx
@@ -27,10 +27,10 @@ const About = () => {
   }, [location]);
 
   return (
-    <div className="about" id="news">
-      <h1 className="aboutTextHead" data-aos="fade-up" data-aos-duration="900" data-aos-delay="200">About Us</h1>
+    <div className="about" id="about">
+      <h1 className="aboutTextHead" data-aos="fade-up" data-aos-duration="900" data-aos-delay="200"><h1>About Us</h1></h1>
       <div className="wrapperAbout">
-        <img className="aboutImg"src={imgAbout} width={900} data-aos="fade-up" data-aos-duration="900" data-aos-delay="300" />
+        <img className="aboutImg"src={imgAbout} data-aos="fade-up" data-aos-duration="900" data-aos-delay="300" />
         <div className="textAbout kanit-regular" data-aos="fade-up" data-aos-duration="900" data-aos-delay="500"><p>Laeta adalah agensi VTuber yang berbasis di Indonesia, yang menaungi berbagai talenta virtual dengan karakter unik dan kreatif. Sejak didirikan, Laeta telah berhasil menarik puluhan ribu penggemar dari seluruh penjuru Indonesia berkat konten hiburan yang menarik seperti live streaming, cover lagu, dan kolaborasi kreatif.</p></div>
       </div>
     </div>
